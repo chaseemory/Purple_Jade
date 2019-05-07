509314edc99ebfe821e4b93fa4f417edb696c1ce
Chase Michael Emory <cemory@linux-lab-023.ece.uw.edu>
2019-05-07 13:03:45 -0700

    updates to common pkg1

--- common/Purple_Jade_pkg.svh
+++ common/Purple_Jade_pkg.svh
@@ -14,6 +14,9 @@ parameter BRANCH_CC_NUM                   = 15;
 
 parameter NUM_FU                          = 7;
 parameter WIDTH_FU                        = $clog2(NUM_FU);
+
+parameter INSTRUCTION_OP_NUM              = 8;
+
 /*   FUNCTIONAL UNITS
 move = 0
 alu = 1
@@ -31,44 +34,44 @@ div = 6
 `define MUL_FU      WIDTH_FU'd5
 `define DEV_FU      WIDTH_FU'd6
 
-
-parameter DECODED_INSTRUCTION_WIDTH       = 2 * $clog2(NUM_ARCH_REG) + 2*WORD_SIZE_P + 
-                                            $clog2(INSTRUCTION_OP_NUM) + $clog2(NUM_FU) + NUM_FLAGS + $clog2(BRANCH_CC_NUM) + 2;
-                                  
 parameter UCODE_WIDTH_P                   = 1 + WIDTH_OP + WIDTH_FU + NUM_FLAGS + WIDTH_DEST + WIDTH_S1 + WIDTH_IMM;
 
 typedef struct packed
 {
-  logic [$clog2(num_reg)-1:0]             dest_id;
-  logic [$clog2(num_reg)-1:0]             source_1;
-  logic [word_size_p-1:0]                 source2_imm;
-  logic [word_size_p-1:0]                 pc;
-  logic [$clog2(num_ops)-1:0]             opcode;
-  logic [$clog2(num_fu)-1:0]              func_unit;
-  logic [num_flags-1:0]                   flags;
-  logic [$clog2(branch_cc_num)-1:0]       bcc_op;
+  logic [$clog2(NUM_ARCH_REG)-1:0]        dest_id;
+  logic [$clog2(NUM_ARCH_REG)-1:0]        source_1;
+  logic [WORD_SIZE_P-1:0]                 source2_imm;
+  logic [WORD_SIZE_P-1:0]                 pc;
+  logic [$clog2(INSTRUCTION_OP_NUM)-1:0]  opcode;
+  logic [$clog2(NUM_FU)-1:0]              func_unit;
+  logic [NUM_FLAGS-1:0]                   flags;
+  logic [$clog2(BRANCH_CC_NUM)-1:0]       bcc_op;
   logic                                   branch_speculation;
   logic                                   w_v;
   logic                                   imm;
   } decoded_instruction_t;
 
-parameter NUM_PHYS_REG                    = 128;
-parameter RENAMED_INSTRUCTION_WIDTH       = 3 * $clog2(NUM_PHYS_REG) + 2*WORD_SIZE_P + $clog2(INSTRUCTION_OP_NUM) + $clog2(NUM_FU) + NUM_FLAGS + $clog2(BRANCH_CC_NUM) + 2 + $clog2(NUM_ARCH_REG);
+parameter DECODED_INSTRUCTION_WIDTH       = $bits(decoded_instruction_t);
 
+parameter NUM_PHYS_REG                    = 128;
 
 typedef struct packed
 {
-  logic [$clog2(num_reg)-1:0]             dest_id;
-  logic [$clog2(num_reg)-1:0]             source_1;
-  logic [word_size_p-1:0]                 source2_imm;
-  logic [word_size_p-1:0]                 pc;
-  logic [$clog2(num_ops)-1:0]             opcode;
-  logic [$clog2(num_fu)-1:0]              func_unit;
-  logic [num_flags-1:0]                   flags;
-  logic [$clog2(branch_cc_num)-1:0]       bcc_op;
+  logic [$clog2(NUM_PHYS_REG)-1:0]        dest_id;
+  logic [$clog2(NUM_PHYS_REG)-1:0]        source_1;
+  logic [WORD_SIZE_P-1:0]                 source2_imm;
+  logic [WORD_SIZE_P-1:0]                 pc;
+  logic [$clog2(INSTRUCTION_OP_NUM)-1:0]  opcode;
+  logic [$clog2(NUM_FU)-1:0]              func_unit;
+  logic [NUM_FLAGS-1:0]                   flags;
+  logic [$clog2(BRANCH_CC_NUM)-1:0]       bcc_op;
+  logic                                   branch_speculation;
   logic                                   w_v;
   logic                                   imm;
-  logic [$clog2(num_reg)-1:0]             freed_reg;   /* those two fileds */
-  logic [$clog2(num_arch_reg)-1:0]        alloc_reg;   /* are used for commit */
+  logic [$clog2(NUM_PHYS_REG)-1:0]        freed_reg;   /* those two fileds */
+  logic [$clog2(NUM_ARCH_REG)-1:0]        alloc_reg;   /* are used for commit */
   } renamed_instruction_t;
-  `endif
+
+parameter RENAMED_INSTRUCTION_WIDTH       = $bits(renamed_instruction_t);
+
+`endif
