cd11992b22c9f734451c18d9aa8e9155d4a746b5
yongqin <yongqw2@yahoo.com>
2019-05-09 10:07:52 -0700

    tests for rob added2

--- rob/obj_dir/Vrob__ALLsup.d
+++
@@ -1,7 +0,0 @@
-Vrob__ALLsup.o: Vrob__ALLsup.cpp Vrob__Trace.cpp \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_vcd_c.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_config.h \
-  Vrob__Syms.h Vrob.h Vrob___024unit.h Vrob__Syms.cpp \
-  Vrob__Trace__Slow.cpp
--- rob/obj_dir/rob_test.d
+++
@@ -1,4 +0,0 @@
-rob_test.o: ../rob_test.cpp Vrob.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilatedos.h
