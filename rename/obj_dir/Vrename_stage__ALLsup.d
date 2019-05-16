38efe9ddeb0618ba60ed0c8239749cd7a9ad3ad3
yongqin <yongqw2@yahoo.com>
2019-05-16 13:23:28 -0700

    unnecessary logics1

--- rename/obj_dir/Vrename_stage__ALLsup.d
+++
@@ -1,8 +0,0 @@
-Vrename_stage__ALLsup.o: Vrename_stage__ALLsup.cpp \
-  Vrename_stage__Trace.cpp \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_vcd_c.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilatedos.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_config.h \
-  Vrename_stage__Syms.h Vrename_stage.h Vrename_stage___024unit.h \
-  Vrename_stage__Syms.cpp Vrename_stage__Trace__Slow.cpp
