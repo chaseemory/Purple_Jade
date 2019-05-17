ccc5ddb5db7c0ee7193a260cbc9333ad9989f8b5
yongqin <yongqw2@yahoo.com>
2019-05-17 13:15:28 -0700

    sb tested1

--- commit_stage/test/sb_test/obj_dir/Vstore_buffer__ALLcls.d
+++
@@ -1,7 +0,0 @@
-Vstore_buffer__ALLcls.o: Vstore_buffer__ALLcls.cpp Vstore_buffer.cpp \
-  Vstore_buffer.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilated_config.h \
-  /usr/local/Cellar/verilator/3.926/share/verilator/include/verilatedos.h \
-  Vstore_buffer__Syms.h Vstore_buffer___024unit.h \
-  Vstore_buffer___024unit.cpp
